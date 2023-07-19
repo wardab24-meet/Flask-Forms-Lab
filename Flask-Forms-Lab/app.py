0d17198561fa7243a1345001eb97f2aaa0bbf07e
wardab24-meet <139688935+wardab24-meet@users.noreply.github.com>
2023-07-19 18:07:19 +0300

    DONNEE FINNALLLYY

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,15 +8,35 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
-password = "123"
+username = "ward abdo"
+password = "2007"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/' , methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if  request.method == 'GET':
+		return render_template('login.html')
+	else:
+		name = request.form['username']
+		password02 = request.form['password']
+	if password == password02 and username == name:
+		return redirect(url_for('home'))
+	else:
+		return render_template('login.html')
+
+@app.route('/home')
+def home():
+		return render_template('home.html' , friends = facebook_friends)
+
+
+@app.route('/friend_exist/<string:name>' , methods=['GET' , 'POST'])
+def friend_exist(name):
+	if name in facebook_friends:
+		F= True
+	else:
+		F=False
+	return render_template('friend_exist.html', F=F , name=name)
 
 
 
